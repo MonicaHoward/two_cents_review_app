c3c202d33b7b1a752db842b08fa7eb930474c339
Monica Howard <jay.m.howard@gmail.com>
2018-06-24 23:18:12 -0500

    Added restaurants controller and new view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,19 @@
 class RestaurantsController < ApplicationController
+
+  def show
+    @restaurant = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(params.require(:restaurant).permit(:name, :street, :city, :state))
+
+    respond_to do |format|
+
+    end
+  end
+
 end
