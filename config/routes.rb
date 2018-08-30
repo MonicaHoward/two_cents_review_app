2f752f224e82b5b4538bd803201d319f31c3e137
Monica Howard <jay.m.howard@gmail.com>
2018-08-29 21:49:08 -0500

    Added main html template and dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
-  root 'restaurants#new'
+  root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
